b09bf218b942914eeffa0669b097ab195cd68562
Emmanuel Toluwanimi Adebayo <42483026+Henadadme@users.noreply.github.com>
2019-09-04 19:39:28 +0100

    Update Solutions.java

--- Solutions.java
+++ Solutions.java
@@ -6,16 +6,7 @@ public class Solutions {
         ArrayDeque<Integer> indexesToCheck = new ArrayDeque<>();
 //        ArrayDeque<Integer> indexesChecked = new ArrayDeque<>();
         indexesToCheck.add(0);
-//        while (currentIndex < game.length) {
-//            if (game[currentIndex + 1] == 0) {
-//                indexesToCheck.add(currentIndex + 1);
-//
-//            }
-//            if (game[currentIndex + leap] == 0) {
-//                indexesToCheck.add(currentIndex + leap);
-//            }
-//
-//        }
+
         int lengthofIndextoCheck = indexesToCheck.size();
         int currentIndex = indexesToCheck.getFirst();
         while (lengthofIndextoCheck > 0) {
@@ -33,9 +24,7 @@ public class Solutions {
                     if (game[currentIndex -1] != 1){
                         indexesToCheck.add(currentIndex-1);
                     }
-//                    if (!(indexesChecked.contains(currentIndex - 1))) {
-//                        indexesToCheck.add(currentIndex - 1);
-//                    }
+//               
                 }
 
             }
@@ -50,38 +39,6 @@ public class Solutions {
                return false;
            }
         }
-//        for (Integer currentIndex : indexesToCheck) {
-//            System.out.println(currentIndex);
-//            if (currentIndex + 1 >= game.length || currentIndex + leap >= game.length) {
-//                return true;
-//            }
-//            if (game[currentIndex + 1] == 0) {
-//                indexesToCheck.add(currentIndex + 1);
-//            }
-//            if (game[currentIndex + leap] == 0) {
-//                indexesToCheck.add(currentIndex + leap);
-//            }
-//            if (currentIndex != 0) {
-//                if (game[currentIndex - 1] == 0) {
-//                    if (!(indexesChecked.contains(currentIndex - 1))) {
-//                        indexesToCheck.add(currentIndex - 1);
-//                    }
-//                }
-//
-//            }
-//
-//            indexesChecked.addLast(currentIndex);
-//            indexesToCheck.removeFirst();
-//            System.out.println("Hey Size" + indexesChecked.size());
-//
-//            for (Integer i : indexesChecked){
-//                System.out.println("Hey Omo"+i);
-//
-//            }
-//            for (Integer j: indexesToCheck){
-//                System.out.println("Hey Baba"+ j);
-//            }
-//        }
         return false;
     }
 
